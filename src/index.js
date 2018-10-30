765318bd09201496f03d8de170da123875604536
Faraj Daoud <faraj.daoud@gmail.com>
2018-10-30 12:33:17 -0400

    Marked properly renders html now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import marked from 'marked';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+marked.setOptions({
+    breaks: true,
+});
+
+const renderer = new marked.Renderer();
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            markdown: initialMarkdown,
+        };
+        this.updatePreviewer = this.updatePreviewer.bind(this);
+    }
+
+    updatePreviewer(event){
+        const markdown = event.target.value;
+        console.log('markdown: ' + markdown);
+        this.setState({
+            markdown: markdown,
+        });
+    }
+
+    render() {
+        return(
+            <div id="app-wrapper">
+                <textarea id="editor" onChange={this.updatePreviewer} value={this.state.markdown}></textarea>
+                <div id="preview" dangerouslySetInnerHTML={{__html:marked(this.state.markdown, { renderer: renderer })}} />
+            </div>
+        );
+    }
+}
+
+const initialMarkdown = 
+`
+# Markdown Previewer
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
