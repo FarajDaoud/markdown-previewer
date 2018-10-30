c9f5605dee939cc69ac1f07cd41a8ee9a07c8d00
Faraj Daoud <faraj.daoud@gmail.com>
2018-10-30 17:02:39 -0400

    changed markup

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ marked.setOptions({
     breaks: true,
 });
 
-const renderer = new marked.Renderer();
-
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -19,10 +17,8 @@ class App extends React.Component {
     }
 
     updatePreviewer(event){
-        const markdown = event.target.value;
-        console.log('markdown: ' + markdown);
         this.setState({
-            markdown: markdown,
+            markdown: event.target.value,
         });
     }
 
@@ -30,7 +26,7 @@ class App extends React.Component {
         return(
             <div id="app-wrapper">
                 <textarea id="editor" onChange={this.updatePreviewer} value={this.state.markdown}></textarea>
-                <div id="preview" dangerouslySetInnerHTML={{__html:marked(this.state.markdown, { renderer: renderer })}} />
+                <div id="preview" dangerouslySetInnerHTML={{__html:marked(this.state.markdown)}} />
             </div>
         );
     }
@@ -38,7 +34,53 @@ class App extends React.Component {
 
 const initialMarkdown = 
 `
-# Markdown Previewer
+# Heading
+
+## Sub-heading
+### And here's some other cool stuff:
+  
+Code between 2 backticks: \`<div></div>\`
+
+\`\`\`
+//multi-line code:
+
+function foo(bar) {
+  if (bar === true) {
+    return 'Hello World';
+  }
+}
+\`\`\`
+  
+Bold Text: **Bold**
+Italic Text:  _Italic_
+Italic and Bold Text: **_Bold and Italic_**
+Strikethrough Text: ~~Hello World~~
+
+Link: [links](https://www.freecodecamp.com)
+Block Quotes: 
+> Block Quotes!
+
+Tables:
+
+First Name | Last Name
+Faraj | Daoud
+Jane | Doe
+
+Unordered Lists: 
+- Item 1
+  - Item 2
+     - Item 3
+        - Item 4
+
+Ordered Lists: 
+1. Item 1
+1. Item 2
+1. Item 3
+- Item 4
+
+Embedded Images:
+
+![Smile on the beach!](https://onehdwallpaper.com/wp-content/uploads/2016/04/Be-Happy-Images-Free-Download-624x468.jpg)
 `;
 
 ReactDOM.render(<App />, document.getElementById('root'));
