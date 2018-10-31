80a93d007af39f1554a4cb4257dc1a018d845005
Faraj Daoud <faraj.daoud@gmail.com>
2018-10-30 21:16:56 -0400

    Finalize project before push to production

--- src/index.js
+++ src/index.js
@@ -34,52 +34,34 @@ class App extends React.Component {
 
 const initialMarkdown = 
 `
-# Heading
+# Header
 
-## Sub-heading
-### And here's some other cool stuff:
+## Sub-header
+
+Link: [freecodecamp.org](https://www.freecodecamp.com)
   
-Code between 2 backticks: \`<div></div>\`
+Inline Code: \`<p>Hello World!</p>\`
 
+Code Block:
 \`\`\`
-//multi-line code:
-
 function foo(bar) {
   if (bar === true) {
     return 'Hello World';
   }
 }
 \`\`\`
-  
-Bold Text: **Bold**
-Italic Text:  _Italic_
-Italic and Bold Text: **_Bold and Italic_**
-Strikethrough Text: ~~Hello World~~
 
-Link: [links](https://www.freecodecamp.com)
-Block Quotes: 
-> Block Quotes!
-
-Tables:
-
-First Name | Last Name
------------- | ------------- 
-Faraj | Daoud
-Jane | Doe
-
-Unordered Lists: 
-- Item 1
-  - Item 2
-     - Item 3
-        - Item 4
-
-Ordered Lists: 
+List: 
 1. Item 1
 1. Item 2
 1. Item 3
-- Item 4
 
-Embedded Images:
+Block Quote: 
+> To be or not to be?
+
+Bold Text: **Bold**
+
+Images:
 
 ![Smile on the beach!](https://onehdwallpaper.com/wp-content/uploads/2016/04/Be-Happy-Images-Free-Download-624x468.jpg)
 `;
